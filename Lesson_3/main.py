e3d3244feb6ad4425b74d9eab8b78ce3ae3a0412
Ortynskyi <75494042+Ortynskyi@users.noreply.github.com>
2020-12-10 18:59:41 +0200

    Add files via upload

---
+++ Lesson_3/main.py
@@ -0,0 +1,10 @@
+a=5
+b=6
+Name_a="a"
+Name_b="b"
+print (Name_a,"=", a, Name_b, "=", b)
+print (Name_a,"^2 =", a**2, Name_b, "^2  =", b**2)
+print (Name_a,"*",Name_b, "=",a*b)
+print (Name_a,"/",Name_b, "=",a/b)
+print (Name_a,"+",Name_b, "=",a+b)
+print (Name_a,"-",Name_b, "=",a-b)
